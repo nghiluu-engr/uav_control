2e8b2d58abf9a612e92b7beacb4d4f823eb79a0d
nghiluu-engr <gianghiluu2007@gmail.com>
2026-09-12 14:20:02 +0700

    push mission_control, offboard_control

---
+++ mission_control.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+from geometry_msgs.msg import PoseStamped, TwistStamped
+from std_msgs.msg import Bool
+from enum import Enum
+
+class MissionState(Enum):
+    WAIT_FOR_POSITION = 0
+    TAKE_OFF = 1
+    MISSION  = 2
+    LANDING_SEARCH = 3
+    DONE = 4
+
+px4_qos = QoSProfile(reliability = ReliabilityPolicy.BEST_EFFORT,
+                         durability = DurabilityPolicy.TRANSIENT_LOCAL,
+                         history = HistoryPolicy.KEEP_LAST,
+                         depth = 1)
+
+ros_qos = 10
+
+class MissionManagerNode(Node):
+    def __init__(self):
+        super().__init__('mission_manager_node')
+
+        #parameters
+        self.declare_parameter('take_off_altitude', -5.0)
+        self.declare_parameter('hold_count_required', 5.0)
+        self.declare_parameter('waypoint',[0.0, 0.0, -5.0,
+                                           -5.0, 0.0, -5.0,
+                                           0.0, -5.0, -5.0])
+
+
+        #value
+        self.take_off_altitude = self.get_parameter('take_off_altitude').value
+        self.hold_count_required = self.get_parameter('hold_count_required').value
+        self.waypoint = self.get_parameter('waypoint').value
+
+        self.current_x = 0.0
+        self.current_y = 0.0
+        self.current_z = 0.0
+
+        self.target_x = 0.0
+        self.target_y = 0.0
+        self.target_z = 0.0
+
+        self.distance_x = 0.0
+        self.distance_y = 0.0
+        self.distance_z = 0.0
+
+        self.current_vx = 0.0
+        self.current_vy = 0.0
+        self.current_vz = 0.0
+
+        self.target_vx = 0.0
+        self.target_vy = 0.0
+        self.target_vz = 0.0
+
+        self.time_start_hover = 0.0
+        self.current_time = 0.0
+
+        self.state = MissionState.WAIT_FOR_POSITION
+
+        self.waypoint_counter = 0
+
+        #timer
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+    #pub
+
+        #position
+        self.target_position_pub = self.create_publisher(PoseStamped,
+                                                    '/mission/target_position',
+                                                    ros_qos)
+        #velocity
+        self.target_velocity_pub = self.create_publisher(TwistStamped,
+                                                        '/mission/target_velocity',
+                                                        ros_qos)
+        #vehicle status
+        self.land_pub = self.create_publisher(Bool,
+                                                '/mission/land',
+                                                ros_qos)
+
+        #sub
+        self.current_position_sub = self.create_subscription(PoseStamped,
+                                                            '/mission/current_position',
+                                                            self.current_position_callback,
+                                                            ros_qos)
+
+        self.current_velocity_sub = self.create_subscription(TwistStamped,
+                                                            '/mission/current_velocity',
+                                                            self.current_velocity_callback,
+                                                            ros_qos)
+
+    #main
+    def timer_callback(self):
+        if self.state != MissionState.LANDING_SEARCH:
+            self.target_position_publisher(self.target_x, self.target_y, self.target_z)
+            print(f'target position: x: {self.target_x}, y: {self.target_y}, z: {self.target_z}')
+            print(f'current position: x: {self.current_x}, y: {self.current_y}, z: {self.current_z}')
+
+            self.target_velocity_publisher(self.target_vx, self.target_vy, self.target_vz)
+            print(f'target velocity: vx: {self.target_vx}, vy: {self.target_vy}, vz: {self.target_vz}')
+            print(f'current velocity: x: {self.current_vx}, y: {self.current_vy}, z: {self.current_vz}')
+
+        if self.state == MissionState.WAIT_FOR_POSITION:
+            self.state = MissionState.TAKE_OFF
+
+        if self.state == MissionState.TAKE_OFF:
+            self.take_off_handle()
+
+        if self.state == MissionState.MISSION:
+            self.handle_mission()
+
+        if self.state == MissionState.LANDING_SEARCH:
+            self.land_publisher(True)
+            self.state = MissionState.DONE
+
+        
+    #mission
+    def take_off_handle(self):
+        self.target_x = 0.0
+        self.target_y = 0.0
+        self.target_z = self.take_off_altitude
+
+        self.target_vx = 0.0
+        self.target_vy = 0.0
+        self.target_vz = -1.5 
+
+        if self.current_z <= 0.9 * self.take_off_altitude:
+            self.state = MissionState.MISSION 
+
+    def handle_mission(self):
+        self.hover()
+
+    def hover(self):
+        if self.time_start_hover == 0.0:
+            self.time_start_hover = self.get_clock().now().nanoseconds / 1e9
+            print('start hovering: ', self.time_start_hover)
+        else:
+            self.current_time = self.get_clock().now().nanoseconds / 1e9
+            print(f'\033[92mcurrent time: \033[0m', self.current_time)
+
+            if self.current_time - self.time_start_hover >= 0.9 * self.hold_count_required \
+                and self.current_z <= 0.9 * self.target_z:
+                    self.time_start_hover = 0.0
+                    self.state = MissionState.LANDING_SEARCH
+                    #self.waypoint_counter += 1
+
+
+    #pub
+    def target_position_publisher(self, x, y, z):
+        target_position_msg = PoseStamped()
+        target_position_msg.pose.position.x = x
+        target_position_msg.pose.position.y = y
+        target_position_msg.pose.position.z = z
+        target_position_msg.header.stamp = self.get_clock().now().to_msg()
+        self.target_position_pub.publish(target_position_msg)
+
+    def target_velocity_publisher(self, vx, vy, vz):
+        target_velocity_msg = TwistStamped()
+        target_velocity_msg.twist.linear.x = vx
+        target_velocity_msg.twist.linear.y = vy
+        target_velocity_msg.twist.linear.z = vz
+        target_velocity_msg.header.stamp = self.get_clock().now().to_msg()
+        self.target_velocity_pub.publish(target_velocity_msg)
+
+    def land_publisher(self, landing_state: Bool):
+        land_msg = Bool()
+        land_msg.data = landing_state
+        self.land_pub.publish(land_msg)
+
+    #sub
+    def current_position_callback(self, msg: PoseStamped):
+        self.current_x = msg.pose.position.x
+        self.current_y = msg.pose.position.y
+        self.current_z = msg.pose.position.z
+        
+
+    def current_velocity_callback(self, msg: TwistStamped):
+        self.current_vx = msg.twist.linear.x
+        self.current_vy = msg.twist.linear.y
+        self.current_vz = msg.twist.linear.z
+        
+
+def main(args = None):
+    rclpy.init(args = args)
+    node = MissionManagerNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ offboard_control.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+from px4_msgs.msg import (VehicleStatus, VehicleCommand, 
+                          OffboardControlMode, VehicleLocalPosition, TrajectorySetpoint)
+from geometry_msgs.msg import PoseStamped, TwistStamped
+from std_msgs.msg import Bool
+
+px4_qos = QoSProfile(reliability = ReliabilityPolicy.BEST_EFFORT,
+                         durability = DurabilityPolicy.TRANSIENT_LOCAL,
+                         history = HistoryPolicy.KEEP_LAST,
+                         depth = 1)
+
+ros_qos = 10
+
+
+class OffboardControlNode(Node):
+    def __init__(self):
+        super().__init__('offboard_control_node')
+
+        #parameter
+        self.declare_parameter("default_target_x", 0.0)
+        self.declare_parameter("default_target_y", 0.0)
+        self.declare_parameter("default_target_z", -5.0)
+
+        #value
+        self.default_target_x = float(self.get_parameter('default_target_x').value)
+        self.default_target_y = float(self.get_parameter('default_target_y').value)
+        self.default_target_z = float(self.get_parameter('default_target_z').value)
+
+        self.target_x = self.default_target_x
+        self.target_y = self.default_target_y
+        self.target_z = self.default_target_z
+
+        self.local_x = 0
+        self.local_y = 0
+        self.local_z = 0
+
+        self.target_vx = 0.0
+        self.target_vy = 0.0
+        self.target_vz = 0.0
+
+        self.current_vx = 0.0
+        self.current_vy = 0.0
+        self.current_vz = 0.0
+
+        self.vehicle_nav_state = None
+
+        #timer
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+    #pub
+
+        #position
+        self.offboard_control_mode_pub = self.create_publisher(OffboardControlMode,
+                                                               '/fmu/in/offboard_control_mode',
+                                                               px4_qos)
+
+        self.target_position_pub = self.create_publisher(TrajectorySetpoint,
+                                                        '/fmu/in/trajectory_setpoint',
+                                                        px4_qos)
+
+        self.current_position_pub = self.create_publisher(PoseStamped,
+                                                          '/mission/current_position',
+                                                          ros_qos)
+
+        #velocity
+        self.current_velocity_pub = self.create_publisher(TwistStamped,
+                                                          '/mission/current_velocity',
+                                                          ros_qos)
+
+        #vehicle command
+        self.vehicle_command_pub = self.create_publisher(VehicleCommand,
+                                                         '/fmu/in/vehicle_command',
+                                                         px4_qos)
+
+
+
+
+    #sub
+
+        #position
+        self.target_position_sub = self.create_subscription(PoseStamped,
+                                                            '/mission/target_position',
+                                                            self.target_position_callback,
+                                                            ros_qos)
+
+        self.local_position_sub = self.create_subscription(VehicleLocalPosition,
+                                                           '/fmu/out/vehicle_local_position_v1',
+                                                           self.local_position_callback,
+                                                           px4_qos)
+
+        #velocity
+        self.target_velocity_sub = self.create_subscription(TwistStamped,
+                                                            '/mission/target_velocity',
+                                                            self.target_velocity_callback,
+                                                            ros_qos)
+
+        self.current_velocity_sub = self.create_subscription(VehicleLocalPosition,
+                                                           '/fmu/out/vehicle_local_position_v1',
+                                                           self.current_velocity_callback,
+                                                           px4_qos)
+
+        
+        #vehicle status
+        self.land_requested_sub = self.create_subscription(Bool,
+                                                           '/mission/land',
+                                                           self.land_requested_callback,
+                                                           ros_qos)
+
+        self.vehicle_status_sub = self.create_subscription(VehicleStatus,
+                                                           '/fmu/out/vehicle_status_v4',
+                                                           self.vehicle_status_callback,
+                                                           px4_qos)
+
+        #flag
+        self.set_point_counter = 0
+        self.armed_sent = False
+
+        self.land_requested = False
+        self.land_command_sent = False
+
+    def timer_callback(self):
+
+        if not self.land_requested:
+            self.land_command_sent = False
+            self.offboard_control_callback()
+            self.set_point_pub()
+        
+        if self.set_point_counter < 10:
+            self.set_point_counter += 1
+
+        if self.set_point_counter == 10 and not self.armed_sent:
+            self.take_off()
+            self.arm()
+            self.armed_sent = True
+            print('arming.......')
+            print('take off.........')
+            
+
+        if self.land_requested and not self.land_command_sent:
+            self.land()
+            if self.vehicle_nav_state == 18:
+                self.land_command_sent = True
+                print('landing......')
+            else:
+                print('trying to land......')
+            print(self.vehicle_nav_state)
+
+
+
+    def local_position_callback(self, msg: VehicleLocalPosition):
+        self.local_x = msg.x
+        self.local_y = msg.y
+        self.local_z = msg.z
+
+        current_position_msg = PoseStamped()
+        current_position_msg.pose.position.x = self.local_x
+        current_position_msg.pose.position.y = self.local_y
+        current_position_msg.pose.position.z = self.local_z
+        current_position_msg.header.stamp = self.get_clock().now().to_msg()
+        self.current_position_pub.publish(current_position_msg)
+
+    def current_velocity_callback(self, msg: VehicleLocalPosition):
+        self.current_vx = msg.vx
+        self.current_vy = msg.vy
+        self.current_vz = msg.vz
+
+        current_velocity_msg = TwistStamped()
+        current_velocity_msg.twist.linear.x = self.current_vx
+        current_velocity_msg.twist.linear.y = self.current_vy
+        current_velocity_msg.twist.linear.z = self.current_vz
+        current_velocity_msg.header.stamp = self.get_clock().now().to_msg()
+        self.current_velocity_pub.publish(current_velocity_msg)
+
+
+    def land_requested_callback(self, msg: Bool):
+        if not self.land_requested and msg.data:
+            self.land_requested = msg.data
+
+    def offboard_control_callback(self):
+        offboard_msg = OffboardControlMode()
+        offboard_msg.position = True
+        offboard_msg.velocity = True
+        offboard_msg.acceleration = False
+        offboard_msg.attitude = False
+        offboard_msg.body_rate = False
+        offboard_msg.thrust_and_torque = False
+        offboard_msg.direct_actuator = False
+
+        offboard_msg.timestamp = self.get_clock().now().nanoseconds // 1000
+        self.offboard_control_mode_pub.publish(offboard_msg)
+
+
+    def target_position_callback(self, msg: PoseStamped):
+        self.target_x = float(msg.pose.position.x)
+        self.target_y = float(msg.pose.position.y)
+        self.target_z = float(msg.pose.position.z)
+
+    def target_velocity_callback(self, msg: TwistStamped):
+        self.target_vx = float(msg.twist.linear.x)
+        self.target_vy = float(msg.twist.linear.y)
+        self.target_vz = float(msg.twist.linear.z)
+
+
+    def set_point_pub(self):
+        set_point_msg = TrajectorySetpoint()
+        set_point_msg.position = [self.target_x, self.target_y, self.target_z]
+        set_point_msg.velocity = [self.target_vx, self.target_vy, self.target_vz]
+        set_point_msg.timestamp = self.get_clock().now().nanoseconds // 1000
+        self.target_position_pub.publish(set_point_msg)
+
+
+    def vehicle_command_callback(self, msg, command, param1 = 0.0, param2 = 0.0):
+        command_msg = msg
+        command_msg.command = command
+        command_msg.param1 = param1
+        command_msg.param2 = param2
+
+        command_msg.target_system = 1
+        command_msg.target_component = 1
+        command_msg.source_system = 1
+        command_msg.source_component = 1
+        command_msg.from_external = True
+
+        command_msg.timestamp = self.get_clock().now().nanoseconds // 1000
+        self.vehicle_command_pub.publish(command_msg)
+
+
+    def vehicle_status_callback(self, msg: VehicleStatus):
+        self.vehicle_nav_state = msg.nav_state
+
+
+    def arm(self):
+        self.vehicle_command_callback(VehicleCommand(), 
+                                      VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 
+                                      param1 = 1.0)
+
+    def disarm(self):
+        self.vehicle_command_callback(VehicleCommand(), 
+                                        VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 
+                                        param1 = 0.0)
+
+    def take_off(self):
+        self.vehicle_command_callback(VehicleCommand(), 
+                                        VehicleCommand.VEHICLE_CMD_DO_SET_MODE, 
+                                        param1 = 1.0, 
+                                        param2 = 6.0)
+
+    def land(self):
+        self.vehicle_command_callback(VehicleCommand(), 
+                                        VehicleCommand.VEHICLE_CMD_NAV_LAND)
+
+    def return_to_home(self):
+        self.vehicle_command_callback(VehicleCommand(), 
+                                        VehicleCommand.VEHICLE_CMD_NAV_RETURN_TO_LAUNCH)
+
+def main(args = None):
+    rclpy.init(args = args)
+    node = OffboardControlNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
